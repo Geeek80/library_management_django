e6c4d64be807f416c561af04749aaa9c2b976dc6
Aakash Tolani <aakashtolani@Air.local>
2019-07-05 21:52:20 +0530

    aaded file upload option and static files

--- django_app/models.py
+++ django_app/models.py
@@ -6,6 +6,7 @@ class mymodel(models.Model):
     eid = models.IntegerField()
     ename = models.CharField(max_length=20)
     email = models.EmailField()
+    image = models.FileField(upload_to='image/')
     # date = models.DateTimeField() # for date type column
     class Meta:
         db_table = 'employ' # name of table to be created
